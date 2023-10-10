4506ec777d44a70937774a56684be517b0fb0bc9
Hunter Halloran <hdh20267@uga.edu>
2023-10-10 18:17:59 -0400

    copilot fixes

--- lua/options.lua
+++ lua/options.lua
@@ -66,7 +66,11 @@ end
 -- no select by setting `"suggest.noselect": true` in your configuration file
 -- NOTE: Use command ':verbose imap <tab>' to make sure Tab is not mapped by
 -- other plugins before putting this into your config
+
 local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_tab_fallback = ""
+
 keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
 keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
 
